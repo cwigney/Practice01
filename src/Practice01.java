6dbf2e7c7ec60e9fea3fef722b7fbf5447162468
Jody 1 <Jody1@192-168-1-15.tpgi.com.au>
2021-02-21 15:57:14 +1100

    This is my second commit, just a simple comment.

--- src/Practice01.java
+++ src/Practice01.java
@@ -1,5 +1,6 @@
 public class Practice01 {
     public static void main(String[] args) {
         System.out.println("This is a practice with Git");
+        // A comment to make a change in Practice01
     }
 }
